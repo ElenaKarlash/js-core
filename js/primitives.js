799fc363f367fc05ff0cfc24d30dee5598160fa5
ElenaKarlash <elenakarlash@hotmail.com>
2021-09-08 14:08:24 +0300

    Homework

--- js/primitives.js
+++ js/primitives.js
@@ -84,15 +84,147 @@ if (age && age !== Infinity) {
 } else {
   console.log(`некорректно: ${age}`);
 }
- 
 
 //Homework
-// обявить переменную score со значением 10 
-//сделать проверку такого значения: является ли оно true, если да, то 
-//проверить и вывести сообщение при значении от 0 до 10 включительно
-//"вы не прощли тестировании"
-//от 10 до 15 вкл "Вы прошли тестипрвание"
+// обявить переменную score со значением 10
+//сделать проверку такого значения: является ли оно true, если да, то
+//проверить и вывести сообщение: при значении от 0 до 10 включительно
+//"вы не прошли тестированиe"
+//от 10 до 15 вкл "Вы прошли тестирoвание"
 //свыше 15 "такой балл не возможен"
-//Переназначит значение Score "0", null, undefined, Infinity, NaN, 15,
+//Переназначить значение Score "0", null, undefined, Infinity, NaN, 15,
 //11, 23, '23'
 
+//console.log(typeof score);
+//console.log(typeof (score - 1));
+//console.log(typeof !!score);
+//console.log(typeof !score);
+
+let score = 10;
+
+if (score) {
+  if (score >= 0 && score <= 10) {
+    console.log("Вы не прошли тестирование");
+  }
+
+  if (score > 10 && score <= 15) {
+    console.log("Вы прошли тестирование");
+  } else {
+    console.log(` такой балл невозможен:${score}`);
+  }
+}
+
+score = null;
+if (score) {
+  if (score >= 0 && score <= 10) {
+    console.log("Вы не прошли тестирование");
+  }
+  if (score > 10 && score <= 15) {
+    console.log("Вы прошли тестирование");
+  }
+  if (score > 15) {
+    console.log(`такой балл невозможен:${score}`);
+  }
+} else {
+  console.log(`не число:${score}`);
+}
+
+score = 0;
+
+if (score) {
+  if (score >= 0 && score <= 10) {
+    console.log("Вы не прошли тестирование");
+  }
+  if (score > 10 && score <= 15) {
+    console.log("Вы прошли тестирование");
+  }
+  if (score > 15) {
+    console.log(`Такой балл невозможен: ${score}`);
+  }
+}
+//} else {
+// console.log(`не число:${score}`);
+//}
+
+score = undefined;
+if (score) {
+  if (score >= 0 && score <= 10) {
+    console.log("Вы прошли тестирование");
+  }
+  if (score > 10 && score <= 15) {
+  }
+  console.log("Вы прошли тестирование");
+} else {
+  console.log(`такой балл невозможен: ${score}`);
+}
+
+score = Infinity;
+if (score || score !== Infinity) {
+  if (score >= 0 && score <= 10) {
+    console.log("Вы не прошли тестирование");
+  }
+  if (score >= 10 && score <= 15) {
+    console.log("Вы прошли тестирование");
+  } else {
+    console.log(`такой балл невозможен: ${score}`);
+  }
+}
+
+score = NaN;
+if (score) {
+  if (score >= 0 && score <= 10) {
+    console.log("Вы не прошли тестирование");
+  }
+  if (score > 10 && score <= 15) {
+    console.log("Вы прошли тестирование");
+  } else {
+    console.log(`такой балл невозможен: ${score}`);
+  }
+}
+
+score = 15;
+if (score) {
+  if (score >= 0 && score <= 10) {
+    console.log("Вы не прошли тестирование");
+  }
+  if (score > 10 && score >= 15) {
+    console.log("Вы прошли тестирование");
+  } else {
+    console.log(`такой балл невозможен:${score}`);
+  }
+}
+
+score = 11;
+if (score) {
+  if (score >= 0 && score <= 10) {
+    console.log("Вы не прошли тестирование");
+  }
+  if (score > 10 && score <= 15) {
+    console.log("Вы прошли тестирование");
+  } else {
+    console.log(`такой балл невозможен: ${score}`);
+  }
+}
+
+score = 23;
+if (score) {
+  if (score >= 0 && score <= 10) {
+    console.log("Вы не прошли тестирование");
+    if (score > 10 && score <= 15) {
+      console.log("Вы прошли тестирование");
+    }
+  } else {
+    `такой балл невозможен: ${score}`;
+  }
+}
+score = "23";
+if (score) {
+  if (score >= 0 && score <= 10) {
+    console.log("Вы прошли тестирование");
+    if (score > 10 && score <= 15) {
+      console.log("Вы прошли тестирование");
+    }
+  } else {
+    `такой балл невозможен: ${score}`;
+  }
+}
